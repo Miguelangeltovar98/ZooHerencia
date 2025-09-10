f5eec7651c85919910d6559654d4e7c35933ae2e
Miguelangeltovar98 <miguel_tovarmu@fet.edu.co>
2025-09-10 09:32:15 -0500

    Actualizacion de las clases en ZooHerencia

--- src/main/java/org/example/ClasesZoo.java
+++ src/main/java/org/example/ClasesZoo.java
@@ -87,28 +87,3 @@ class Caja<T> {
         return objeto;
     }
 }
-
-public class ZooHerencia {
-    public static void main(String[] args) {
-        Consultorio consultorio = new Consultorio("Calle 123");
-        Veterinaria vet = new Veterinaria(consultorio);
-
-        Perro perro = new Perro("Firu", new Collar("Rojo"));
-        Gato gato = new Gato("Misu");
-
-        vet.atender(perro);
-        vet.atender(gato);
-
-        Animal a = new Perro("Rocky", new Collar("Azul"));
-
-        if (a instanceof Perro) {
-            Perro p = (Perro) a;
-            p.mostrarCollar();
-        }
-
-        Caja<Animal> caja = new Caja<>();
-        caja.guardar(gato);
-        Animal guardado = caja.obtener();
-        vet.atender(guardado);
-    }
-}
